1f42670b79ff6df9f024e1a17fddedc5b215bbe1
Alexandre Morey <alexander006_9@hotmail.com>
2022-03-18 19:53:59 +0000

    reviewed my code

--- lib/logs_formatter.rb
+++ lib/logs_formatter.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Formatter
     module Logs
 
@@ -6,14 +8,16 @@ module Formatter
             # get the logs and store them into an array
             @logs = File.readlines(logs_data)
             # @logs is an array of strings
+            # foreach
         end
-        # puts "#{Logs.get_data('webserver.log')}"
+        # puts "#{Logs.get_data('webserver.log2')}"
 
 
         # list of webpages with most page views ordered from most pages views to less page views
         def self.most_viewed_webpages(logs_data)
             @logs_array = get_data(logs_data)
-            
+            # check the elements of the array, and make sure this is the proper format (nil, spaces, ...)
+            # binding.pry
             pages_views_count = {}
             @logs_array.each do |log|
                 page_and_ip = log.split(" ")
@@ -28,27 +32,40 @@ module Formatter
         end
 
         puts "the most viewed page"
-        puts "#{Logs.most_viewed_webpages('webserver.log')}"
+        puts "#{Logs.most_viewed_webpages('webserver.log2')}"
         
         #list of webpages with most unique page views also ordered
         def self.webpages_with_most_unique_views(logs_data)
             #if ip address unique, unique view. So remove duplicate
             @logs_array = get_data(logs_data)
-            
             pages_views_count = {}
             @logs_array.each do |log|
                 ip_pages = log.split(" ")
-                pages_views_count[ip_pages[1]] = ip_pages[0]
+                # pages_views_count[ip_pages[0]] = ip_pages[1]
+
+                if pages_views_count.key?(ip_pages[0])
+                    pages_views_count[ip_pages[0]] << ip_pages[1]
+                else
+                    pages_views_count[ip_pages[0]] = [ip_pages[1]]
+                end
+
+
             end
+
+
+            
+
 
             # from this hash (pages_views_count), I will create a hash of pages / uniques vues
             # to do so, I need to count the number of times a value is repeated in a Hash
-            pages_views_count.group_by(&:last).map { |v, a| [v, a.count] }.to_h.sort
+            pages_views_count.map { |v, a| [v, a.uniq.count] }.to_h.sort
+            # binding.pry
+
 
         end
 
         puts "the page with the most unique views"
-        puts "#{Logs.webpages_with_most_unique_views('webserver.log')}"
+        puts "#{Logs.webpages_with_most_unique_views('webserver.log2')}"
 
     end
 end
